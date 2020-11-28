5699788314fd463453219c4de799ef9c37ea23d9
Z <mrzhouyongjian@163.com>
2020-11-28 23:34:08 +0800

    调整下单精度

--- create_order_yhkgtjs_ask_member1.rb
+++ create_order_yhkgtjs_ask_member1.rb
@@ -9,7 +9,7 @@ m = Member.new(email: @email)
 
 @count = 0
 loop do
-   is_continue = m.create_order  true,  (1+rand(1000)/100),  9 + rand(100)/50.0.round(4)
+   is_continue = m.create_order  true,  (1+rand(1000)/100),  (9 + rand(100)/50.0).round(3)
    p @count
    break if is_continue == false
 end
--- create_order_yhkgtjs_bid_member2.rb
+++ create_order_yhkgtjs_bid_member2.rb
@@ -10,7 +10,7 @@ m = Member.new(email: @email)
 
 @count = 0
 loop do
-   is_continue = m.create_order  false,  (1+rand(1000)/100),  9 + rand(100)/50.0.round(4)
+   is_continue = m.create_order  false,  (1+rand(1000)/100),  (9 + rand(100)/50.0).round(4)
    p @count
    break if is_continue == false
 end
